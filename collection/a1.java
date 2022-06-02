234dd53ff98dd280cdf75d9a1e0d38387ce26273
Divyashin <divyashindeshinde8@gmail.com>
2022-06-02 13:22:23 +0530

    Initial commit

---
+++ collection/a1.java
@@ -0,0 +1,16 @@
+package collection;
+
+import java.util.ArrayList;
+
+public class a1 {
+	ArrayList a=new ArrayList()
+	a.add(10);
+	a.add("hii");
+	a.add(false);
+	a.add(null);
+	a.add(10);
+	a.add("byy");
+	a.add(null);
+	System.out.println(a);
+	
+}
---
+++ p2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class p2 {
+	ArrayList a=new ArrayList()
+	a.add(10);
+	a.add("hii");
+	a.add(false);
+	a.add(null);
+	a.add(10);
+	a.add("byy");
+	a.add(null);
+	System.out.println(a);
+	
+}
